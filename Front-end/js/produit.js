8202c4833eb7ebfc9ae8ad3cc7bede74e031adfe
Hyde <heyd.stephane@gmail.com>
2021-06-20 16:20:16 +0200

    ✨ Set LocalStorage

--- Front-end/js/produit.js
+++ Front-end/js/produit.js
@@ -19,7 +19,7 @@ await fetch(apiURL) // En rajoutant la variable urlID on demande que le produit 
       data.imageUrl,
       data.lenses,
       data.name,
-      data.price 
+      data.price
     );
 
     cameras.displayArticle();
@@ -28,20 +28,33 @@ await fetch(apiURL) // En rajoutant la variable urlID on demande que le produit 
     alert("Erreur !", error);
   });
 
-// Mise en place du panier
+// Mise en place du panier ------------------------------------------------------------------------------
 let orderBtn = document.querySelector(".btn-order"); // On cible le bouton "ajouter au panier"
 
 orderBtn.addEventListener("click", () => {
+  // La fonction va rajouter l'article dans le Localstorage lors du clic sur le bouton "Ajouter au panier"
+  let cartContent = JSON.parse(localStorage.getItem("articleStored")) || []; // L'objet ci-dessous sera récupéré dans un tableau
+  let newArticle = {
+    // Objet créé pour sélectionner les informations les informations inhérentes à l'article sélectionné
+    id: cameras.id,
+    name: cameras.name,
+    price: cameras.price / 100 + "€",
+    lense: cameras.selectCustomization,
+    number: quantity,
+  };
 
-let newArticle = { // Objet créé pour regrouper les informations qui seront affichées dans le panier
-  id: cameras.id,
-  name: cameras.name,
-  price: cameras.price / 100 + "€",
-  lense: cameras.selectCustomization,
-  number: quantity,
-};
-console.log(newArticle)
+  let alreadyInCart = cartContent.findIndex(
+    // Renvoie l'index du premier élément du tableau
+      ((item) => item.id == newArticle.id) && // Arguments mis en place pour ne pas créer de doublons pour les id et options identiques
+      ((item) => item.lense == newArticle.lense)
+  );
+
+  if (alreadyInCart == -1) {
+    // Si l'article n'est pas encore présent (index à -1) on le rajoute au tableau
+    cartContent.push(newArticle);
+  } else {
+    cartContent[alreadyInCart].number += quantity; // Sinon on incrémente uniquement sa quantité
+  }
+
+  localStorage.setItem("articleStored", JSON.stringify(cartContent));
 });
-
-
-
